bf168e0910a2bba17cb449defe5adb37c0ca5ded
rodisho <rafiodisho@gmail.com>
2022-11-27 14:27:36 -0500

    modified google data structure in addition to data processing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import construction from './construction.png'
 import './App.css';
 import {Chart} from "react-google-charts";
+import React, { useState, useEffect } from 'react';
+import Data from './google-data.json'
 
 
 const data = [
@@ -18,8 +20,33 @@ const options = {
     },
 };
 
-
 function App() {
+
+    let timeStamp = []
+    let finalResults = []
+    let trendingKeywords = []
+
+
+    useEffect(() => {
+        let objSize = Object.keys(Data).length;
+        for (let i = 1; i <= objSize; i++){
+            console.log(" DATA of index", i ," " , Data[i].time)
+
+
+            for(let k = 0; k < Data[i].results.length; k++){
+            console.log(" trending name ", Data[i].results[k].name)
+                trendingKeywords[k] = (Data[i].results[k].name);
+            }
+            timeStamp[i] = (Data[i].time);
+            // timeStamp[i].push(trendingKeywords)
+        }
+        finalResults.push(trendingKeywords)
+        // finalResults.push(timeStamp)
+        console.log("THIS IS FINAL OBJECT " , timeStamp)
+        console.log("THIS IS FINAL OBJECT " , finalResults)
+    });
+
+
     return (
         <div className="App">
             <header className="App-header">
