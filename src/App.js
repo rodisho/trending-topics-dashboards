c245b8fe0446df2e5c03847a6db97031aec4e8ed
rodisho <rafiodisho@gmail.com>
2022-11-27 19:17:10 -0500

    Initial grid work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import construction from './construction.png'
 import './App.css';
 import {Chart} from "react-google-charts";
+import MainBody from './components/mainBody'
 
 
-const data = [
-    ["Year", "Sales", "Expenses", "Profit"],
-    ["2014", 1000, 400, 200],
-    ["2015", 1170, 460, 250],
-    ["2016", 660, 1120, 300],
-    ["2017", 1030, 540, 350],
-];
 
 const options = {
     chart: {
@@ -27,15 +21,7 @@ function App() {
                     Google Charts
                 </h3>
             </header>
-            <div className={"App-body"}>
-                <Chart
-                    chartType="Line"
-                    width="100%"
-                    height="400px"
-                    data={data}
-                    options={options}
-                />
-            </div>
+            <MainBody></MainBody>
 
         </div>
     );
