ad9abf41c0bc6cfc385a88cb350fbc984960321b
Zane Pelletier <zpelleti@umich.edu>
2022-12-03 18:47:36 -0500

    Fixing data structure and rendering table.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import construction from './construction.png'
 import './App.css';
 import {Chart} from "react-google-charts";
 import React, { useState, useEffect } from 'react';
-import GoogleData from './db/google_trends_formatted.json'
-import TwitterData from './db/twitter_trends_formatted.json'
-import VTData from './db/virus_total_results_formatted.json'
+import GoogleData from './db/google_trends_formatted.json';
+import TwitterData from './db/twitter_trends_formatted.json';
+import VTData from './db/virus_total_results_formatted.json';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -27,33 +27,55 @@ function App() {
 
     let trendingKeywords = [];
 
-    useEffect(() => {
-        let objSize = Object.keys(GoogleData).length;
-        for (let i = 1; i <= objSize; i++){
-            let tempResult = [];
-            for(let k = 0; k < Data[i].results.length; k++){
-                tempResult.push(Data[i].results[k].name);
+
+    // Format Google Keywords and normalize data
+    trendingKeywords = [];
+    let objSize = Object.keys(GoogleData).length;
+    for(let i = 1; i < objSize; i++){
+        for(let k = 1; k < GoogleData[i]['results'].length; k++) {
+            let vtResults = [];
+            let GoogleTerm = GoogleData[i]['results'][k];
+            for(let j = 1; j < Object.keys(VTData).length; j++) {
+                if(GoogleData[i]['_id'] === VTData[j]['origin']) {
+                    for(const [key, value] of Object.entries(VTData[j]['results'])) {
+                        if(key.includes(GoogleTerm['name'])){
+                            if(value['suspicious'] > 0 || value['malicious'] > 0) {
+                                vtResults.push(value);
+                            }
+                        }
+                    }
+                }
             }
-            formatedKeywords[Data[i]._id] = tempResult
-            formatedKeywords[Data[i]._id].push(Data[i].time)
-
+            trendingKeywords.push({'keyword': GoogleData[i]['results'][k]['name'], 'platform': 'Google', 'timestamp': GoogleData[i]['time'], 'malicious_urls': vtResults})
         }
-        console.log(" ***** ", formatedKeywords);
-        console.log(" ***** ", typeof(formatedKeywords));
-        formatedKeywords.splice(1,formatedKeywords.length-1)
-        console.log(" ===> ", formatedKeywords);
-
-        // const arr = Array.from(formatedKeywords);
-        //
-        // console.log(" #### ", arr)
-    });
-
+    }
+    // Format Twitter Keywords and normalize data
+    objSize = Object.keys(TwitterData).length;
+    for(let i = 1; i < objSize; i++){
+        for(let k = 1; k < TwitterData[i]['results'].length; k++) {
+            let vtResults = [];
+            let GoogleTerm = TwitterData[i]['results'][k];
+            for(let j = 1; j < Object.keys(VTData).length; j++) {
+                if(TwitterData[i]['_id'] === VTData[j]['origin']) {
+                    for(const [key, value] of Object.entries(VTData[j]['results'])) {
+                        if(key.includes(GoogleTerm['name'])){
+                            if(value['suspicious'] > 0 || value['malicious'] > 0) {
+                                vtResults.push(value);
+                            }
+                        }
+                    }
+                }
+            }
+            trendingKeywords.push({'keyword': TwitterData[i]['results'][k]['name'], 'platform': 'Google', 'timestamp': TwitterData[i]['time'], 'malicious_urls': vtResults})
+        }
+    }
+    
 
     return (
         <div className="App">
             <header className="App-header">
                 <h3>
-                    Google Charts
+                    Trending Topics
                 </h3>
             </header>
             <div className={"App-body"}>
@@ -61,28 +83,28 @@ function App() {
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>ID</TableCell>
+                                <TableCell>Keyword</TableCell>
                                 <TableCell align="right">Time</TableCell>
-                                <TableCell align="right">Name</TableCell>
+                                <TableCell align="right">Platform</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {
-
-                                formatedKeywords.map((formatedKeyword) => (
+                            
+                            
+                            {trendingKeywords.map(el => 
                                 <TableRow
-                                    key={formatedKeyword.id}
+                                    key={el.keyword}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
-                                    <TableCell key={formatedKeyword.name} component="th" scope="row">
-                                        {formatedKeyword.name}
+                                    <TableCell key={el.keyword} component="th" scope="row">
+                                        {el.keyword}
                                     </TableCell>
-                                    <TableCell align="right">RAFI</TableCell>
-                                    <TableCell align="right">RAFI</TableCell>
+                                    <TableCell align="right">{el.timestamp}</TableCell>
+                                    <TableCell align="right">{el.platform}</TableCell>
                                 </TableRow>
-                            ))
-
-                            }
+                            )}
+                            
+                            
                         </TableBody>
                     </Table>
                 </TableContainer>
