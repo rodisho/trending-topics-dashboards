34d65d43be37434097bfa033f9f8d16a3a202f02
Zane Pelletier <zpelleti@umich.edu>
2022-12-06 17:54:23 -0500

    Adding Google Maps API key back in.

--- src/App.js
+++ src/App.js
@@ -198,7 +198,7 @@ function App() {
                         width="100%"
                         height="400px"
                         data={countrydata}
-                        // mapsApiKey="AIzaSyDAV8XexOIHjguK2nHxQv1ihqjZUtexhNk"
+                        mapsApiKey="AIzaSyDAV8XexOIHjguK2nHxQv1ihqjZUtexhNk"
                     />
                 </div>
                 {/*<MainBody></MainBody>*/}
